7dacdb54f85d720e0af82245fb8e932c06f7ea6a
Everton Walker <everton.walker@hotmail.com>
2022-02-05 13:25:34 -0200

    base do projeto

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './user.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+
+        // Here we create the data from DTO
+        const user = User.create(createUserDto);
+        // Save
+        await user.save();
+        // Now we delete a single property
+        delete user.password;
+        // and return them.
+        return user;
+    }
+
+    async showById(id: string): Promise<User> {
+        const user = await User.findOne(id);
+        delete user.password;
+        return user;
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        });
+
+        if (user) {
+            return user;
+        } else {
+            throw new NotFoundException('Dados incorretos verifique-os.');
+        }
+    }
+
+
+
+}
